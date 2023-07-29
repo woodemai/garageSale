01d74ea006570ace7523ec92aef9e13d18f289a7
woodemai <n.savchenkoo73@gmail.com>
2023-07-30 00:56:52 +0300

    complete auth logic

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import Input from "@/app/components/Input";
 import Button from "@/app/components/Button";
@@ -10,12 +9,21 @@ import {BsGithub, BsGoogle} from "react-icons/bs";
 import {LiaYandexInternational} from "react-icons/lia";
 import axios from 'axios';
 import {toast} from "react-hot-toast";
-import {signIn} from 'next-auth/react';
+import {signIn, useSession} from 'next-auth/react';
+import {useRouter} from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER';
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/main')
+            console.log('Authenticated')
+        }
+    }, [session?.status]);
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN') {
             setVariant('REGISTER')
@@ -42,6 +50,7 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             axios.post('/api/register', data)
+                .then(() => signIn('credentials',{...data, redirect:false}))
                 .catch(() => toast.error('Something went wrong!'))
                 .finally(() => setIsLoading(false));
         }
@@ -62,6 +71,7 @@ const AuthForm = () => {
     }
     const socialAction = (action: string) => {
         setIsLoading(true);
+
         signIn(action, {redirect: false})
             .then((callback) => {
                 if (callback?.error) {
@@ -69,6 +79,7 @@ const AuthForm = () => {
                 }
                 if (callback?.ok && !callback?.error) {
                     toast.success("Logged in");
+                    router.push('/main')
                 }
             }).finally(() => setIsLoading(false));
     }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
 import bcrypt from 'bcrypt';
 import NextAuth, {AuthOptions} from 'next-auth';
-import {PrismaAdapter} from '@next-auth/prisma-adapter';
-import prisma from '@/app/libs/prismadb';
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProver from 'next-auth/providers/google'
 import YandexProvider from 'next-auth/providers/yandex'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import {PrismaAdapter} from '@next-auth/prisma-adapter';
+
+import prisma from '@/app/libs/prismadb';
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
